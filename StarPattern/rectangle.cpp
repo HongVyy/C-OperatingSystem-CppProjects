470904fd6ecbb5f50253478c0b94182a318ec4f6
HongVy <92348180+HongVyy@users.noreply.github.com>
2023-11-03 23:23:17 -0700

    Add files via upload

--- StarPattern/rectangle.cpp
+++ StarPattern/rectangle.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+	int i, j, rows, columns;
+	char ch;
+	cout << "\nPlease the Number of Rows    =  ";
+	cin >> rows;
+	cout << "\nEnter the Number of Columns =  ";
+	cin >> columns;
+	cout << "\nEnter Symbol to Print  =  ";
+	cin >> ch;	
+	cout << "\n-----Rectangle Pattern-----\n";
+
+	while(i < rows){
+    	j = 0; 
+        while(j < columns){
+           cout << ch;
+           j++;
+        }
+        cout << "\n";
+        i++;
+	}
+ 	return 0;
+}
